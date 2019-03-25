f708417434b1f306641f4b408f38b6498ae7007a
Cronwel <noelirias@gmail.com>
2019-03-25 16:52:30 -0500

    Set up of routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  namespace api do
+    namespace vi do
+      resources :workouts do
+        resources :exercises
+      end
+    end
+  end
+
+
+
 end
